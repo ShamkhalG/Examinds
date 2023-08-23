1ab240450311ede370223d39e3100810aef642a0
ShamkhalG <shamkhalguliyev83@gmail.com>
2023-08-23 22:51:53 +0400

    MoreInfo was changed.
    1. Font Raleway was added
    
    2. CSS was added to MoreInfo
    
    3. Background Image was changed due to contrast issues
    
    4. Navigation and MoreInfo components are now on the same line on the left side of the page

--- frontend/src/components/main/MoreInfo.tsx
+++ frontend/src/components/main/MoreInfo.tsx
@@ -1,9 +1,11 @@
 export default function MoreInfo() {
   return (
     <div className = "moreInfoBody">
-      <h2> Онлайн подготовка к экзаменам ГЭЦ </h2>
-      <p> Открыты наборы на 2024 год </p>
-      <button> Узнать подробности </button>
+      <h2 className = "MITitle"> <span className = "orangeTT">ОНЛАЙН</span> <br />
+      ПОДГОТОВКА К <br />
+      ЭКЗАМЕНАМ <span className = "orangeTT">ГЭЦ</span> </h2>
+      <p className = "MIOpen"> Открыты наборы на 2024 год </p>
+      <button className = "MIMore"> Узнать подробности </button>
     </div>
   )
 }
