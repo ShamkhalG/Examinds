874218f0e855c2e6e69df265fc72e78b4b8e68d0
ShamkhalG <shamkhalguliyev83@gmail.com>
2024-07-30 23:13:14 +0400

    Added the Navigation panel (mobile version is not fully ready)

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <Navigation />
+    <!-- TODO Rest of the home page -->
+  </div>
+</template>
+
+<script>
+import Navigation from './home/Navigation.vue';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+/* Desktop styles */
+@media (min-width: 768px) {
+  .home {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
+}
+
+/* Mobile styles */
+@media (max-width: 767px){ 
+  .home {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+}
+
+.home {
+  background-image: url('../assets/backgrounds/main_bg.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/home/Navigation.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="nav">
+    <button class="navregular">Главная</button>
+    <button class="navregular">О нас</button>
+    <button class="navregular">Преподаватели</button>
+    <button class="navsignin">ВОЙТИ</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navigation"
+}
+</script>
+
+<style>
+/* Changing the width of the navigation panel */
+@media (min-width: 1100px) {
+  .nav {
+    width: 60%;
+  }
+}
+
+@media (min-width: 767px) and (max-width: 1100px) {
+  .nav {
+    width: 80%;
+  }
+}
+
+/* Desktop styles */
+@media(min-width: 767px) {
+  .navregular, .navsignin {
+    font-size: 1.25rem;
+  }
+
+  .navsignin {
+    padding-left: 3.375rem;
+    padding-right: 3.375rem;
+    padding-top: 1.125rem;
+    padding-bottom: 1.125rem;
+  }
+}
+
+/* TODO Mobile styles */
+@media (max-width: 767px){ 
+  .nav {
+    width: 100%;
+  }
+
+  .navregular {
+    font-size: 0.875rem;
+  }
+  
+  .navsignin {
+    font-size: 0.768rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 0.688rem;
+    padding-bottom: 0.688rem;
+  }
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1rem;
+  height: 4rem;
+}
+
+.navregular {
+  background: none;
+  border: none;
+  color: #FFFFFF;
+  cursor: pointer;
+  font-family: 'Inter-Regular';
+}
+
+.navsignin {
+  background-color: rgb(217, 217, 217, 0.3);
+  border: solid 2px;
+  border-radius: 10px;
+  border-color: linear-gradient(to right, rgba(255, 255, 255, 0.55), #F96F16);
+  color: #F96F16;
+  cursor: pointer;
+  font-family: 'Inter-SemiBold';
+}
+</style>
