3e46be2faba9af61dc962a55677be8ed8f201248
ShamkhalG <shamkhalguliyev83@gmail.com>
2025-08-05 15:41:31 +0200

    Home page is fully ready.
    Practice section completed for desktop version.

--- src/components/home/Practice.vue
+++ src/components/home/Practice.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="practiceDiv bg-cover bg-center flex flex-col justify-center items-center w-full">
-    <h3 class="font-geologicaBold text-[1.65rem] mt-8 mb-[-0.5rem]">Практика</h3>
-    <h4 class="font-geologicaMedium text-[1.17rem] mb-2">в передовых лабораториях</h4>
+  <div class="bg-practice_bg-mobile lg:bg-practice_bg-pc bg-cover bg-center flex flex-col justify-center items-center w-full">
+    <h3 class="text-[1.65rem] lg:text-[5rem] lg:text-minds mt-8 lg:mt-[5.2rem] mb-[-0.5rem] lg:mb-[-1.7rem] font-geologicaBold">Практика</h3>
+    <h4 class="text-[1.17rem] lg:text-[2.4rem] mb-2 font-geologicaMedium">в передовых лабораториях</h4>
     
-    <p class="text-center text-[0.8rem] py-4 px-10 mb-5
-      border border-2 border-minds rounded-md font-interMedium"
+    <p class="text-center text-[0.8rem] lg:text-[1.25rem] py-4 px-10 mb-5 lg:w-[61%]
+      border border-2 lg:border-[3px] border-minds rounded-md font-interMedium"
     >
       У <span class="font-interBold">вас появится шанс</span> открыть мир <br class="lg:hidden" />
-      науки через практику в одной из <br class="lg:hidden"/>
+      науки через <br class="hidden lg:block" /> практику в одной из <br class="lg:hidden"/>
       лучших лабораторий страны!
     </p>
 
     <!-- Lab photos -->
-    <img class="w-[84%] mb-[4.75rem]" src="@/assets/images/practice/lab_photos.png" alt="Lab photos"/>
+    <img class="w-[84%] lg:w-[61%] mb-[4.75rem] lg:mb-[5.5rem]" src="@/assets/images/practice/lab_photos.png" alt="Lab photos"/>
 
     <!-- Buttons -->
-    <div class="flex flex-row justify-end mb-6 w-[270px]">
+    <div class="flex flex-row justify-end mb-6 w-[270px] lg:w-[760px]">
       <!-- Left button -->
       <button @click="curr_index -= 1" 
-        class="w-12 h-12 bg-contain bg-center bg-no-repeat"
+        class="w-12 lg:w-[70px] h-12 lg:h-[70px] bg-contain bg-center bg-no-repeat"
         :disabled="curr_index === 0"
         :style="{ backgroundImage: `url(${curr_index === 0 ? left_arrow_disabled : left_arrow_enabled})` }">
       </button>
 
       <!-- Right button -->
       <button @click="curr_index += 1" 
-        class="w-12 h-12 ml-2 bg-contain bg-center bg-no-repeat"
+        class="w-12 lg:w-[70px] h-12 lg:h-[70px] ml-2 bg-contain bg-center bg-no-repeat"
         :disabled="curr_index === 1"
         :style="{ backgroundImage: `url(${curr_index === 0 ? right_arrow_enabled : right_arrow_disabled})` }">
       </button>
     </div>
 
     <!-- Videos -->
-    <div class="videoDiv bg-cover bg-center flex flex-col justify-center items-center mb-16 w-[300px] h-[175px]">
+    <div class="bg-video-frame-small lg:bg-video-frame-large bg-cover bg-center flex flex-col justify-center items-center mb-16 w-[300px] lg:w-[970px] h-[175px] lg:h-[564px]">
       <iframe
-        class="w-[285px] h-[161px]"
+        class="w-[285px] lg:w-[921px] h-[161px] lg:h-[520px]"
         :src="curr_index === 0 ? 'https://www.youtube.com/embed/QusWbUPXuqQ' : 'https://www.youtube.com/embed/uukmHfraa68'"
         title="YouTube video player"
         frameborder="0"
@@ -44,10 +44,12 @@
     </div>
 
     <!-- Note about Discovery Week -->
-    <div class="dwnoteDiv flex flex-col justify-center mb-16 w-full bg-cover bg-center h-[100px]">
-      <p class="ml-auto mr-[2.5rem] text-[0.65rem] font-interSemiBold">
+    <div class="bg-dwnote_bg-small lg:bg-dwnote_bg-large bg-cover bg-center flex flex-col justify-center 
+    mb-16 lg:mb-[11rem] w-full lg:w-[820px] h-[100px] lg:h-[150px]"
+    >
+      <p class="ml-auto mr-[2.5rem] lg:mr-[2rem] text-[0.65rem] lg:text-[1rem] font-interSemiBold">
         Откройте для себя, что вас <br class="lg:hidden"/>
-        ждёт на <span class="text-minds font-interExtraBold">Discovery Week в UFAZ</span> <br class="lg:hidden"/>
+        ждёт на <span class="text-minds font-interExtraBold">Discovery Week в UFAZ</span> <br />
         – <span class="font-interExtraBold">загляните внутрь лабораторий</span> <br class="lg:hidden"/>
         и почувствуйте атмосферу науки!
       </p>
@@ -71,17 +73,3 @@ export default {
   }
 };
 </script>
-
-<style>
-  .practiceDiv {
-    background-image: url('@/assets/backgrounds/practice_bg.png');
-  }
-
-  .videoDiv {
-    background-image: url('@/assets/images/practice/video_frame.png');
-  }
-
-  .dwnoteDiv {
-    background-image: url('@/assets/images/practice/dwnote_bg.png');
-  }
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,14 @@ module.exports = {
         geologicaMedium: ['Geologica-Medium', 'sans-serif'],
         geologicaBold: ['Geologica-Bold', 'sans-serif']
       },
+      backgroundImage: {
+        'practice_bg-mobile': "url('@/assets/backgrounds/practice_bg.png')",
+        'practice_bg-pc': "url('@/assets/backgrounds/practice_bg_pc.png')",
+        'video-frame-small': "url('@/assets/images/practice/video_frame.png')",
+        'video-frame-large': "url('@/assets/images/practice/video_frame_pc.png')",
+        'dwnote_bg-small': "url('@/assets/images/practice/dwnote_bg.png')",
+        'dwnote_bg-large': "url('@/assets/images/practice/dwnote_bg_pc.png')",
+      },
       colors: {
         minds: '#F96F16'
       }
